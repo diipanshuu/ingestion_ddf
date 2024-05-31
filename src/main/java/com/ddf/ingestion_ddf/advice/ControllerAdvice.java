698a94687e19f04613c5c9f11c641f5e86c91f86
diipanshuu <diipanshuu@gmail.com>
2024-06-01 01:21:42 +0530

    feat: Implement ControllerAdvice for exception handling

---
+++ src/main/java/com/ddf/ingestion_ddf/advice/ControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.ddf.ingestion_ddf.advice;
+
+import com.ddf.ingestion_ddf.request.mappers.ErrorDTO;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@org.springframework.web.bind.annotation.ControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorDTO> handleMethodArgumentTypeMismatch(){
+        ErrorDTO errorDto = new ErrorDTO();
+        errorDto.setMessage("Invalid order_by field. Please specify a valid value like: MODIFIED_DATE");
+
+        return new ResponseEntity<>(errorDto, HttpStatusCode.valueOf(400));
+    }
+}
---
+++ src/main/java/com/ddf/ingestion_ddf/request/mappers/ErrorDTO.java
@@ -0,0 +1,10 @@
+package com.ddf.ingestion_ddf.request.mappers;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorDTO {
+    private String message;
+}
