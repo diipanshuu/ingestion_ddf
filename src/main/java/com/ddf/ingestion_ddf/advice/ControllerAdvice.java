b054b75235e1dece96e46a50be9c41a3163cdf7e
diipanshuu <diipanshuu@gmail.com>
2024-06-01 15:59:58 +0530

    feat: Implement Global Exception Handler

--- src/main/java/com/ddf/ingestion_ddf/advice/ControllerAdvice.java
+++ src/main/java/com/ddf/ingestion_ddf/advice/ControllerAdvice.java
@@ -1,8 +1,7 @@
+/*
 package com.ddf.ingestion_ddf.advice;
 
-import com.ddf.ingestion_ddf.request.mappers.ErrorDTO;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,14 +9,6 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
-//    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-//    public ResponseEntity<String> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex){
-////        ErrorDTO errorDto = new ErrorDTO();
-////        errorDto.setMessage("Invalid order_by field. Please specify a valid value like: modifiedDate");
-//
-////        return new ResponseEntity<>(errorDto, HttpStatusCode.valueOf(400));
-//        return new ResponseEntity<>("Validation error: " + ex.getBindingResult().getFieldError().getDefaultMessage(), HttpStatus.BAD_REQUEST);
-//    }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<String> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
@@ -31,3 +22,46 @@ public class ControllerAdvice {
         return new ResponseEntity<>("Validation error: " + ex.getBindingResult().getFieldError().getDefaultMessage(), HttpStatus.BAD_REQUEST);
     }
 }
+*/
+
+package com.ddf.ingestion_ddf.advice;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+@org.springframework.web.bind.annotation.ControllerAdvice
+public class ControllerAdvice {
+
+    private MessageSource messageSource;
+
+    public ControllerAdvice(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex, Locale locale) {
+        String errorMessage = String.format("The parameter '%s' with value '%s' could not be converted to type '%s'.",
+                ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex, Locale locale) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = messageSource.getMessage(error, locale);
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
+
